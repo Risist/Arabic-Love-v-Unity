0ae43f839c6f97aafdbe91152ff6c847dd07a3f4
Dante004 <bartek.bk@tlen.pl>
2016-09-20 21:28:19 +0200

    Add files via upload

---
+++ Script/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject woman;
+    public float Wait,Wait1;
+    public Transform spawnV;
+    public float startWait = 1;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+                Instantiate(woman, spawnV.position, spawnV.rotation);
+                yield return new WaitForSeconds(Random.Range(Wait,Wait1));
+
+        }
+    }
+
+}
+
+
+
