3b4fec031fea10543a1bb0bf5f344f6d1b8da3e8
Dante004 <bartek.bk@tlen.pl>
2016-09-20 21:05:09 +0200

    Add files via upload

---
+++ Meczet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meczet : MonoBehaviour {
+
+    
+
+    void Start()
+    {
+        
+    }
+
+    void OntriggerEnter()
+    {
+
+    }
+}
