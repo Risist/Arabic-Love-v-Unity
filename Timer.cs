8109ae554cf1e3ead8e34553aa3aebb1be86c33e
Risist <dmnn@op.pl>
2016-09-20 20:54:07 +0200

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[System.Serializable]
+public class Timer
+{
+    public float actualTime = 0;
+    public float cd = 1;
+
+    public void update()
+    {
+        actualTime += Time.deltaTime;
+    }
+
+    public void restart()
+    {
+        actualTime = 0;
+    }
+
+    public bool isReady( float cd)
+    {
+        return actualTime >= cd;
+    }
+    public bool isReady()
+    {
+        return isReady(cd);
+    }
+    public bool isReadyRestart(float cd)
+    {
+        if( actualTime >= cd )
+        {
+            restart();
+            return true;
+        }
+        return false;
+    }
+    public bool isReadyRestart()
+    {
+        return isReadyRestart(cd);
+    }
+}
