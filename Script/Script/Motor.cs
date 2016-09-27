6ca0eaccda21097188bb2d6abdcb65f50b904b23
Risist <dmnn@op.pl>
2016-09-27 21:18:32 +0200

    Add files via upload

---
+++ Script/Script/Motor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+    public float movementSpeed;
+    public float movementSpeedFall;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        movementSpeed -= movementSpeedFall * Time.deltaTime;
+        transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Script/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    MoveTo moveTo;
+
+    public void computeMousePos()
+    {
+        float camDistance = Camera.main.transform.position.y - transform.position.y;
+        moveTo.destination = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDistance));
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        moveTo = GetComponent<MoveTo>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButton("Fire1"))
+            computeMousePos();
+    }
+}
